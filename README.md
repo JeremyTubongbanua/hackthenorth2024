680fe14161cdc0c213987e7cb20e935fdce3f4a0
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-09-15 02:38:14 -0400

    feat: verbose mode and lots of documentation

--- README.md
+++ README.md
@@ -2,5 +2,28 @@
 
 This is the repository for my Hack The North 2024 submission.
 
-Our project is "Secure Remote Access into a Jetson Nano in an air gapped environment without exposing ports"
+- [Emily Lai](https://github.com/emilyirenelai)
+- [Jeremy Tubongbanua](https://github.com/JeremyTubongbanua)
 
+## Summary
+
+Our project is "Secure Remote Access into a Jetson Nano in an air gapped environment without exposing ports." In this hack, we offer features ranging from allowing you to recover from a loss of Internet connection whilst remaining a remote access connection to your device without that device having any open ports (at the level of a 192.168 address).
+
+## How It Works
+
+Our hack involves a Python tool that utilizes Atsign's NoPorts as a core dependency for secure remote access into devices without having to expose any ports. Atsign's NoPorts is a great solution because it is fully open source and allows for things like pulling their docker image so that we can locally run our very own NoPorts relay point, fully enabling us to utilize thier tech even in an air gapped setting.
+
+Atsign is also great because all encryption keys are generated at the edge and all traffic is encrypted with them. So no central server can see any of the very top secret things that a person may be doing while remote accessing another machine.
+
+## Features
+
+- **Recovery and Compatible in Air Gapped Environment** - if the user happens to both 1. be connected to the Internet and 2. be on the same network as the Jetson Nano and relay point, then if the Internet connection is lost, we can fall back to using the local method of remote access via the local raspberry pi relay point that is within the same network as both your own device and the Jetson Nano.
+- **Sensitive Device in Literally Unhackable** - the Jetson Nano, in this case, is completely unhackable because it has no open ports, even at the level of a 192.168 address. This means that if a malicious person were to try to attack the Jetson Nano through the Internet, they can't because there are no exposed ports. Also, if a person were to come in-person to a secure facility and tried to access that device through the local network, they would also be unable to because the device has no open ports at the level of a 192.168 address.
+- **Zero Trust** - nor the client nor the server have to trust the central server with any secrets or keys. This hack follows a zero trust architecture because the encryption keys are at the device level. We chose to go with a Raspberry Pi, Jetson Nano, and a MacBook for our demo devices because they are all devices that are highly capable of encryption. In a typical setting, there would be some sort of central body or institution that could see all of your data and have access to it. But in this hack, the tunnel in its establishment and during its entire session is fully peer-to-peer.
+- **Secure** - the NoPorts relay point is a secure way to remote access devices because it is encrypted with keys that are on the device. This means that even if a malicious person were to try to attack the relay point, they would not be able to decrypt the traffic because the keys are on the device.
+- **Smart CLI Tool** - the Python tool that we have created is a smart CLI tool that allows for the user to easily remote access their device. We provide 1. a config file that allows users of the tool to provide the atSigns that they own. 2. We provide an easy to use interface such that all scenarios are covered and possible to be ran in 1 command with the flags that you can pick and choose. For example, `./connect.py --cli` offers a fully interactive experience and `./connect.py --override-with-no-internet -p 3389 -l 12332` allows for the user to override default settings and skip the CLI steps and do everything in one quick succession.
+- **Utility** - we believe our tool offers a lot of utility for those in IT or people who are constantly attending things like sensitive government machines. You can use our tool to remote access your device while still using the same remote access tool that you are familiar with. Just run the tool in the background and then you can go back to using something like Microsoft RDP. You can have the joy of doing things the way that you are comfortable with while also having the luxury of knowing that your device is secure and that you don't have to trust anyone in order to do it.
+
+## Demo
+
+4 minute video of the tool in action - [https://youtu.be/lrKfHAbEC_E](https://youtu.be/lrKfHAbEC_E)
--- tool/connect.py
+++ tool/connect.py
@@ -8,8 +8,6 @@ import argparse
 import configparser
 
 # Check if we are connected to the Internet. Based on that, we can decide if we should either 1. connect through Internet rendezvous or 2. connect locally (and assume that we are on the same network as the Jetson Nano).
-
-
 def check_internet():
     try:
         requests.get("https://www.google.com", timeout=5)
@@ -17,13 +15,14 @@ def check_internet():
     except (requests.ConnectionError, requests.Timeout):
         return False
 
-
 def load_config(config_file):
     config = configparser.ConfigParser()
     config.read(config_file)
     return config
 
-
+# run either
+# 1. local command (no Internet, tunnel through relay server on local network, you can only access this device as long as you are on the local network)
+# 2. Internet command (tunnel through a relay server on the Internet, you can access your device from any network on Internet)
 def run_command(config, use_cli):
     parser = argparse.ArgumentParser(
         description="Run NPT commands with Internet/local connection options.")
@@ -39,86 +38,87 @@ def run_command(config, use_cli):
                         help='Override to force using local method')
     parser.add_argument('--cli', action='store_true',
                         help='Use CLI input instead of config file')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Enable verbose output for the npt command')
 
     args = parser.parse_args()
 
     if use_cli:
         # handle the possiblilty of the user using it like `./connect.py --cli -p 22` -> in this scenario, the user is providing the remote port but not the local port, and maybe we could still ask
         # something like `./connect.py --cli -p 22 -l 12332 --rh localhost would be the same as `./connect.py -p 22 -l 12332 --rh localhost`, which is an interesting case to handle, but is intended
-        port = args.p
-        if not port:
-            port = input(
-                "Enter the port number for the -p flag (Press Enter for default 22): ") or "22"
-        local_port = args.l
-        if not local_port:
-            local_port = input(
-                "Enter the local port number for the -l flag (Press Enter for default 12332): ") or "12332"
-        rh_flag = args.rh
-        if not rh_flag:
-            rh_flag = input(
-                "Enter the value for --rh flag (Press Enter for default \"localhost\"): ") or "localhost"
+        port = args.p or input(
+            "Enter the port number for the -p flag (Press Enter for default 22): ") or "22"
+        local_port = args.l or input(
+            "Enter the local port number for the -l flag (Press Enter for default 12332): ") or "12332"
+        rh_flag = args.rh or input(
+            "Enter the value for --rh flag (Press Enter for default \"localhost\"): ") or "localhost"
     else:
-        # default to config.ini values
         port = args.p or config.get('Internet', 'internet_remote_port')
         local_port = args.l or config.get('Internet', 'internet_local_port')
         rh_flag = args.rh or config.get('Internet', 'internet_remote_host')
 
+    verbose = args.verbose
+
     if args.override_with_internet:
-        # in this 1st scenario, the user is forcing the internet method
+        # forcefully use the Internet method of doing things
         print("Override detected: Forcing Internet method.")
-        run_internet_command(config, port, local_port, rh_flag)
+        run_internet_command(config, port, local_port, rh_flag, verbose)
     elif args.override_with_no_internet:
-        # in this 2nd scenario, the user is forcing the local method, we shall assume that the user is on the same network as the Jetson Nano
+        # forcefully use the local method of doing things
         print("Override detected: Forcing local method.")
-        run_local_command(config, port, local_port, rh_flag)
+        run_local_command(config, port, local_port, rh_flag, verbose)
     else:
-        # in this 3rd scenario, we shall check if they are connected to the Internet ourselves
+        # 3rd scenario is we should check ourselves for good user experience
         if check_internet():
             print("Internet connection detected. Using Internet method.")
-            run_internet_command(config, port, local_port, rh_flag)
+            run_internet_command(config, port, local_port, rh_flag, verbose)
         else:
             print("No Internet connection detected. Using local method.")
-            run_local_command(config, port, local_port, rh_flag)
+            run_local_command(config, port, local_port, rh_flag, verbose)
 
-
-def run_internet_command(config, port, local_port, rh_flag):
+def run_internet_command(config, port, local_port, rh_flag, verbose):
     print("Connecting via Internet...")
-
     internet_from = config.get('Internet', 'internet_from')
     internet_to = config.get('Internet', 'internet_to')
     internet_rvd = config.get('Internet', 'internet_rvd')
     internet_device = config.get('Internet', 'internet_device')
 
-    command = f"~/.local/bin/npt -f {internet_from} -t {internet_to} -r {
-        internet_rvd} -d {internet_device} -K -T 0 -p {port} -l {local_port}"
+    command = f"~/.local/bin/npt -f {internet_from} -t {internet_to} -r {internet_rvd} -d {internet_device} -K -T 0 -p {port} -l {local_port}"
+
     if rh_flag:
         command += f" --rh {rh_flag}"
+
+    if verbose:
+        command += " -v"
+
     subprocess.run(command, shell=True)
     print("Monitoring Internet connection...")
 
+    # we should attempt to fall back and recover to local connection if the Internet connection is lost
     while True:
         if not check_internet():
             print("Disconnected from the Internet. Switching to local connection.")
-            run_local_command(config, port, local_port, rh_flag)
+            run_local_command(config, port, local_port, rh_flag, verbose)
             break
         time.sleep(5)
 
-
-def run_local_command(config, port, local_port, rh_flag):
+def run_local_command(config, port, local_port, rh_flag, verbose):
     print("Running local connection...")
-
     air_gapped_from = config.get('AirGapped', 'air_gapped_from')
     air_gapped_to = config.get('AirGapped', 'air_gapped_to')
     air_gapped_rvd = config.get('AirGapped', 'air_gapped_rvd')
     air_gapped_device = config.get('AirGapped', 'air_gapped_device')
     air_gapped_root_domain = config.get('AirGapped', 'air_gapped_root_domain')
 
-    command = f"~/.local/bin/npt -f {air_gapped_from} -t {air_gapped_to} -r {air_gapped_rvd} -d {
-        air_gapped_device} --root-domain {air_gapped_root_domain} -K -T 0 -p {port} -l {local_port}"
+    command = f"~/.local/bin/npt -f {air_gapped_from} -t {air_gapped_to} -r {air_gapped_rvd} -d {air_gapped_device} --root-domain {air_gapped_root_domain} -K -T 0 -p {port} -l {local_port}"
+
     if rh_flag:
         command += f" --rh {rh_flag}"
-    subprocess.run(command, shell=True)
 
+    if verbose:
+        command += " -v"
+
+    subprocess.run(command, shell=True)
 
 if __name__ == "__main__":
     config_file = "config.ini"
